02bd83b720d10a27e38db21bb838d2271c318770
Professor MAD <professormad@proton.me>
2024-10-15 14:06:10 -0400

    app.css added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import "./App.css";
+
 function App() {
     return(
-        <div>Hello World</div>
+        <div>
+            <button className="books">Books</button>
+        </div>
     );
 };
 
