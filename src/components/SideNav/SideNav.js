b0894dd2e5b0be9d9d1627d6392051102937705f
Professor MAD <professormad@proton.me>
2024-10-18 13:30:03 -0400

    sidenav html and css initital stuff done

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,22 +1,28 @@
-import { FaHome, FaCog } from 'react-icons/fa'; // Importing icons
+import { FaHome, FaCog, FaHistory } from 'react-icons/fa'; // Importing icons
 import "./SideNav.css"
+import { FaCartShopping } from 'react-icons/fa6';
 
 function SideNav() {
     return (
         <div className="side-nav-wrapper">
             <div className="logo-container">
+                <img className='dcomply-image-styles' src='/images/DComply-logo.png' alt='Dcomply logo' />
                 DComply
             </div>
             <div className="side-nav-option">
-                <FaHome className='icon-styles'/> My Modules
+                <FaCog className='icon-styles'/>
+                 My Modules
             </div>
             <div className="side-nav-option">
-                <FaCog className='icon-styles'/> My Workshops
+                <FaHome className='icon-styles'/>
+                 My Workshops
             </div>
             <div className="side-nav-option">
+                <FaHistory className='icon-styles'/>
                 My History
             </div>
             <div className="side-nav-option">
+                <FaCartShopping className='icon-styles'/>
                 Shop Modules
             </div>
         </div>
