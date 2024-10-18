61051f618b6c5de2c32dca16c284772791b8060c
Professor MAD <professormad@proton.me>
2024-10-18 12:53:42 -0400

    react icons imported after svg.js debacle

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,7 +1,24 @@
+import { FaHome, FaCog } from 'react-icons/fa'; // Importing icons
+import "./SideNav.css"
+
 function SideNav() {
     return (
-        <div>
-            sideNav
+        <div className="side-nav-wrapper">
+            <div className="logo-container">
+                DComply
+            </div>
+            <div className="side-nav-option">
+                <FaHome className='icon-styles'/> My Modules
+            </div>
+            <div className="side-nav-option">
+                <FaCog className='icon-styles'/> My Workshops
+            </div>
+            <div className="side-nav-option">
+                My History
+            </div>
+            <div className="side-nav-option">
+                Shop Modules
+            </div>
         </div>
     );
 }
