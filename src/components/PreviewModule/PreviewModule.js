0122b681e833f3de20b6fec21de7042e07d741da
Professor MAD <professormad@proton.me>
2024-12-17 11:10:39 -0500

    Styling: Monkey and Title Text turn blue when hovered. Done with state bc parent div hovering triggers it.

--- src/components/PreviewModule/PreviewModule.js
+++ src/components/PreviewModule/PreviewModule.js
@@ -1,20 +1,37 @@
+import { useState } from "react";
 import "./PreviewModule.css";
 
 function PreviewModule() {
+  const [isMonkeyHovered, setIsMonkeyHovered] = useState(false);
+  const [isTitleHovered, setIsTitleHovered] = useState(false);
   return (
-    <div className="preview-module-container">
+    <div
+      className="preview-module-container"
+      onMouseEnter={() => {
+        setIsTitleHovered(true);
+        setIsMonkeyHovered(true);
+      }}
+      onMouseLeave={() => {
+        setIsTitleHovered(false);
+        setIsMonkeyHovered(false);
+      }}
+    >
       <div className="header">
         <div className="status">Assigned</div>
       </div>
       <div className="preview-icon-holder">
         <img
           className="monkey-reading"
-          src="/images/monkey-reading.png"
+          src={
+            isMonkeyHovered
+              ? "/images/monkey-reading-dark.png"
+              : "/images/monkey-reading.png"
+          }
           alt="monkey-reading"
         />
       </div>
       <div className="title-and-description-container">
-        <div className="title">Title</div>
+        <div className={`title ${isTitleHovered ? "active" : ""}`}>Title</div>
         <div className="description"></div>
       </div>
     </div>
