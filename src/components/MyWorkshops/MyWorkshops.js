66a82bb093cd9eca5cf90a975e4a784cdbecfc08
Professor MAD <professormad@proton.me>
2024-12-20 09:47:02 -0500

    Structure and Styling: No new modules alert message added.

--- src/components/MyWorkshops/MyWorkshops.js
+++ src/components/MyWorkshops/MyWorkshops.js
@@ -4,6 +4,20 @@ function MyWorkshops() {
   return (
     <div className="my-workshops-parent">
       <div className="home-button">My Current Workshops</div>
+      <div className="preview-holder">
+        <div className="no-current-modules">
+          <div className="top-div-no-current-modules">
+            You do not have any assigned workshops!
+          </div>
+          <div className="bottom-div-no-current-modules">
+            Contact&nbsp;
+            <a href="mailto:professormad@proton.me" className="email-link">
+              professormad@proton.me
+            </a>
+            &nbsp;if you feel this is an error.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
